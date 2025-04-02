68ef793563c0df694ba7bd4ad6224ace0bd43d6f
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-04-02 17:16:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import Sidebar from './scenes/global/Sidebar'
 import Dashboard from "./scenes/dashboard";
 /* import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";  */
 
+import Geography from "./scenes/geography";  */
+import Bar from "./scenes/bar";
 import Calendar from './scenes/calendar/calendar'
+import Contacts from "./scenes/contacts";
+import FAQ from "./scenes/faq";
 
 function App() {
 
@@ -30,16 +30,20 @@ function App() {
             <Topbar/>
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              
               {/* <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
+              
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
+      
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
+              
               <Route path="/geography" element={<Geography />} /> */}
               <Route path="/calendar" element={<Calendar />} /> 
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/faq" element={<FAQ />} />
             </Routes>
           </main>
         </div>
