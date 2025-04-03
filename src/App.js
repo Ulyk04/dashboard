d7f3d038cb6904bf2057548d722c932d06f3a971
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-04-03 13:32:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Sidebar from './scenes/global/Sidebar'
 import Dashboard from "./scenes/dashboard";
 /* import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
-import Form from "./scenes/form";
+
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 
@@ -15,6 +15,7 @@ import Bar from "./scenes/bar";
 import Calendar from './scenes/calendar/calendar'
 import Contacts from "./scenes/contacts";
 import FAQ from "./scenes/faq";
+import Form from "./scenes/form";
 
 function App() {
 
@@ -34,7 +35,7 @@ function App() {
               {/* <Route path="/team" element={<Team />} />
               
               <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
+             
       
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
@@ -44,6 +45,7 @@ function App() {
               <Route path="/bar" element={<Bar />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/faq" element={<FAQ />} />
+              <Route path="/form" element={<Form />} />
             </Routes>
           </main>
         </div>
