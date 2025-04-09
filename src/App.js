922c824157591328094e8439d6ddc1b4cff7689a
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-04-09 20:11:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,58 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar";
+import { ColorModeContext , useMode } from "./theme";
+import { CssBaseline , ThemeProvider } from "@mui/material";
+import { Routes , Route } from "react-router-dom";
+import Topbar from './scenes/global/Topbar'
+import Sidebar from './scenes/global/Sidebar'
 import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
+/* import Team from "./scenes/team";
+
+
+
+
+ */
 import Bar from "./scenes/bar";
+import Calendar from './scenes/calendar/calendar'
+import Contacts from "./scenes/contacts";
+import FAQ from "./scenes/faq";
 import Form from "./scenes/form";
+import Geography from "./scenes/geography"; 
+import Invoices from "./scenes/invoices";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar";
 
 function App() {
-  const [theme, colorMode] = useMode();
-  const [isSidebar, setIsSidebar] = useState(true);
+
+  const [theme , colorMode] = useMode();
 
   return (
     <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme = {theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar isSidebar={isSidebar} />
+          <Sidebar />
           <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
+            <Topbar/>
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
+              
+              {/* <Route path="/team" element={<Team />} />
+              
+            
+             
+      
+             
+             
+              
+              */}
+              <Route path="/calendar" element={<Calendar />} /> 
               <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
+              <Route path="/contacts" element={<Contacts />} />
               <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/form" element={<Form />} />
               <Route path="/geography" element={<Geography />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/pie" element={<Pie />} />
             </Routes>
           </main>
         </div>
